7014e7b25d527740e8249fb32cc6f7c76d4de7d8
MorskoyToster <verhovskii_ivan@mail.ru>
2020-11-02 22:15:30 +0500

    Initial commit

---
+++ isPositiveOrNegative.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.java_one.lesson_1.Verhovskii_IA;
+
+public class isPositiveOrNegative {
+
+    public static void main(String[] args) {
+        int number = 25;
+            if (number >= 0) {
+            System.out.println("Number is positive");
+        }
+            else {
+                System.out.println("Number is negative");
+            }
+
+    }
+}
